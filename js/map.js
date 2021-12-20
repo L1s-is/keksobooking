1e3db93bebcf527fef55fc601d67aaeba7bc6f0a
Елизавета <86424489+L1s-is@users.noreply.github.com>
2021-12-20 23:04:27 +0500

    Добавила разделение кода на несколько модулей с функциями по IIFE

--- js/map.js
+++ js/map.js
@@ -1,194 +1,195 @@
 'use strict';
 
-/*let map = document.querySelector(".map")*/
-let template = document.querySelector("template")
-let newTemplate = template.content.querySelector('.map__card');
-let mapPin = template.content.querySelector(".map__pin")
-let findCreateMapPins
-let mapPinMain = document.querySelector(".map__pin--main")
-let mapFilters = document.querySelector(".map__filters")
-let mapFiltersSelects = mapFilters.querySelectorAll("select")
-let mapFiltersFieldset = mapFilters.querySelectorAll("fieldset")
-let listOfPins = document.querySelector(".map__pins")
-let adFormAnnoucement = document.querySelector(".ad-form")
-let adFormFieldsets = adFormAnnoucement.querySelectorAll("fieldset")
-let addressInput = adFormAnnoucement.querySelector("#address")
-let mapFiltersContainer = map.querySelector(".map__filters-container")
-const pinWidth = 50
-const pinHeight = 70
-/*const listOfAvatars = getPathAvatar()
-const listOfTitles = ["Большая уютная квартира", "Маленькая неуютная квартира",
-  "Огромный прекрасный дворец", "Маленький ужасный дворец",
-  "Красивый гостевой домик", "Некрасивый негостеприимный домик",
-  "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"]
-const listOfTypes = ['palace', 'flat', 'house', 'bungalo']
-const listCheckinsCheckouts = ["12:00", "13:00", "14:00"]
-const listOfFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"]
-const listOfPhotos = ["https://o0.github.io/assets/images/tokyo/hotel1.jpg", "https://o0.github.io/assets/images/tokyo/hotel2.jpg", "https://o0.github.io/assets/images/tokyo/hotel3.jpg"]
-let listObjects = createArrayOfObjects(listOfAvatars, listOfTitles, listOfTypes, listCheckinsCheckouts, listOfFeatures, listOfPhotos)*/
-const valueTypes = {
-  palace: 'Дворец',
-  flat: 'Квартира',
-  house: 'Дом',
-  bungalo: 'Бунгало'
-}
+let adFormAnnoucement
 
-function createMapPin(element) {
-  let newPin = mapPin.cloneNode(true)
-  let newPinImg = newPin.querySelector("img")
-  newPin.style.left = element.location.x - pinWidth / 2 + "px"
-  newPin.style.top = element.location.y - pinHeight + "px"
-  newPinImg.src = element.author.avatar
-  newPinImg.alt = element.offer.title
-  console.log(newPin)
-  return newPin
-}
-
-function createFragment(arr) {
-  let fragment = document.createDocumentFragment()
-  for (let i = 0; i < arr.length; i++) {
-    /*let pinElement = createMapPin(arr[i])*/
-    fragment.appendChild(createMapPin(arr[i]))
+(function () {
+  let template = document.querySelector("template")
+  let newTemplate = template.content.querySelector('.map__card');
+  let mapPin = template.content.querySelector(".map__pin")
+  let findCreateMapPins
+  let mapPinMain = document.querySelector(".map__pin--main")
+  let mapFilters = document.querySelector(".map__filters")
+  let mapFiltersSelects = mapFilters.querySelectorAll("select")
+  let mapFiltersFieldset = mapFilters.querySelectorAll("fieldset")
+  let listOfPins = document.querySelector(".map__pins")
+  adFormAnnoucement = document.querySelector(".ad-form")
+  let adFormFieldsets = adFormAnnoucement.querySelectorAll("fieldset")
+  let addressInput = adFormAnnoucement.querySelector("#address")
+  let mapFiltersContainer = map.querySelector(".map__filters-container")
+  const pinWidth = 50
+  const pinHeight = 70
+  const valueTypes = {
+    palace: 'Дворец',
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalo: 'Бунгало'
   }
-  listOfPins.appendChild(fragment)
-}
-const mainPinWidth = 64
-const mainPinHeight = 80
-function getAddressFormAnnoucement() {
-  let coordinatX = Math.round(mapPinMain.offsetLeft + mainPinWidth / 2)
-  let coordinatY = Math.round(mapPinMain.offsetTop + mainPinHeight)
-  let addressInputValue = coordinatX + ", " + coordinatY
-  return addressInputValue
-}
 
-function formAnnoucementActiveHandler() {
-  adFormAnnoucement.classList.remove('ad-form--disabled')
-}
-
-function formElementActiveHandler(disabledArr) {
-  for (let i = 0; i < disabledArr.length; i++) {
-    disabledArr[i].disabled = false
+  function createMapPin(element) {
+    let newPin = mapPin.cloneNode(true)
+    let newPinImg = newPin.querySelector("img")
+    newPin.style.left = element.location.x - pinWidth / 2 + "px"
+    newPin.style.top = element.location.y - pinHeight + "px"
+    newPinImg.src = element.author.avatar
+    newPinImg.alt = element.offer.title
+    console.log(newPin)
+    return newPin
   }
-}
 
-function createFeatures(arr) {
-  let fragment = document.createDocumentFragment()
-  for (let i = 0; i < arr.offer.features.length; i++) {
-    let features = document.createElement("li")
-    features.className = "popup__feature popup__feature--" + arr.offer.features[i]
-    fragment.appendChild(features)
+  function createFragment(arr) {
+    let fragment = document.createDocumentFragment()
+    for (let i = 0; i < arr.length; i++) {
+      /*let pinElement = createMapPin(arr[i])*/
+      fragment.appendChild(createMapPin(arr[i]))
+    }
+    listOfPins.appendChild(fragment)
   }
-  return fragment
-}
 
-function createAnnouncement(element) {
-  let newAnnouncement = newTemplate.cloneNode(true)
-  newAnnouncement.querySelector(".popup__avatar").src = element.author.avatar
-  newAnnouncement.querySelector(".popup__title").textContent = element.offer.title
-  newAnnouncement.querySelector(".popup__text--address").textContent = element.offer.address
-  newAnnouncement.querySelector(".popup__text--price").textContent = element.offer.price + " ₽/ночь"
-  newAnnouncement.querySelector(".popup__type").textContent = valueTypes[element.offer.type]
-  newAnnouncement.querySelector(".popup__text--capacity").textContent = element.offer.rooms + " комнаты для " + element.offer.guests + " гостей"
-  newAnnouncement.querySelector(".popup__text--time").textContent = "Заезд после " + element.offer.checkin + ", выезд до " + element.offer.checkout
-  newAnnouncement.querySelector(".popup__features").textContent = ""
-  let features = createFeatures(element)
-  newAnnouncement.querySelector(".popup__features").appendChild(features)
-  newAnnouncement.querySelector(".popup__description").textContent = element.offer.description
-  newAnnouncement.querySelector(".popup__photo").src = element.offer.photos
-  newAnnouncement.classList.add("visually-hidden")
-  map.insertBefore(newAnnouncement, mapFiltersContainer)
-  return newAnnouncement
-}
+  const mainPinWidth = 64
+  const mainPinHeight = 80
 
-function createMapPinAnnoucements() {
-  for (let i = 0; i < listObjects.length; i++) {
-    createAnnouncement(listObjects[i])
+  function getAddressFormAnnoucement() {
+    let coordinatX = Math.round(mapPinMain.offsetLeft + mainPinWidth / 2)
+    let coordinatY = Math.round(mapPinMain.offsetTop + mainPinHeight)
+    let addressInputValue = coordinatX + ", " + coordinatY
+    return addressInputValue
   }
-}
 
-function PopupActiveHandler(annoucement, mapPin) {
-  annoucement.classList.remove("visually-hidden")
-  mapPin.classList.add("map__pin--active")
-}
+  function formAnnoucementActiveHandler() {
+    adFormAnnoucement.classList.remove('ad-form--disabled')
+  }
 
-function PopupInactiveHandler(annoucement, mapPin) {
-  annoucement.classList.add("visually-hidden")
-  mapPin.classList.remove("map__pin--active")
-}
+  function formElementActiveHandler(disabledArr) {
+    for (let i = 0; i < disabledArr.length; i++) {
+      disabledArr[i].disabled = false
+    }
+  }
 
-function mapPinClickHandler(mapCards) {
-  for (let i = 1; i < findCreateMapPins.length; i++) {
-    findCreateMapPins[i].addEventListener("click", function () {
-      for (let j = 1; j < findCreateMapPins.length; j++) {
-        PopupInactiveHandler(mapCards[j - 1], findCreateMapPins[j])
-      }
-      PopupActiveHandler(mapCards[i - 1], findCreateMapPins[i])
-    })
-    let popupClose = mapCards[i - 1].querySelector(".popup__close")
-    popupClose.addEventListener("click", function () {
-      PopupInactiveHandler(mapCards[i - 1], findCreateMapPins[i])
-    })
-    window.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27) {
+  function createFeatures(arr) {
+    let fragment = document.createDocumentFragment()
+    for (let i = 0; i < arr.offer.features.length; i++) {
+      let features = document.createElement("li")
+      features.className = "popup__feature popup__feature--" + arr.offer.features[i]
+      fragment.appendChild(features)
+    }
+    return fragment
+  }
+
+  function createAnnouncement(element) {
+    let newAnnouncement = newTemplate.cloneNode(true)
+    newAnnouncement.querySelector(".popup__avatar").src = element.author.avatar
+    newAnnouncement.querySelector(".popup__title").textContent = element.offer.title
+    newAnnouncement.querySelector(".popup__text--address").textContent = element.offer.address
+    newAnnouncement.querySelector(".popup__text--price").textContent = element.offer.price + " ₽/ночь"
+    newAnnouncement.querySelector(".popup__type").textContent = valueTypes[element.offer.type]
+    newAnnouncement.querySelector(".popup__text--capacity").textContent = element.offer.rooms + " комнаты для " + element.offer.guests + " гостей"
+    newAnnouncement.querySelector(".popup__text--time").textContent = "Заезд после " + element.offer.checkin + ", выезд до " + element.offer.checkout
+    newAnnouncement.querySelector(".popup__features").textContent = ""
+    let features = createFeatures(element)
+    newAnnouncement.querySelector(".popup__features").appendChild(features)
+    newAnnouncement.querySelector(".popup__description").textContent = element.offer.description
+    newAnnouncement.querySelector(".popup__photo").src = element.offer.photos
+    newAnnouncement.classList.add("visually-hidden")
+    map.insertBefore(newAnnouncement, mapFiltersContainer)
+    return newAnnouncement
+  }
+
+  function createMapPinAnnoucements() {
+    for (let i = 0; i < listObjects.length; i++) {
+      createAnnouncement(listObjects[i])
+    }
+  }
+
+  function PopupActiveHandler(annoucement, mapPin) {
+    annoucement.classList.remove("visually-hidden")
+    mapPin.classList.add("map__pin--active")
+  }
+
+  function PopupInactiveHandler(annoucement, mapPin) {
+    annoucement.classList.add("visually-hidden")
+    mapPin.classList.remove("map__pin--active")
+  }
+
+  function mapPinClickHandler(mapCards) {
+    for (let i = 1; i < findCreateMapPins.length; i++) {
+      findCreateMapPins[i].addEventListener("click", function () {
+        for (let j = 1; j < findCreateMapPins.length; j++) {
+          PopupInactiveHandler(mapCards[j - 1], findCreateMapPins[j])
+        }
+        PopupActiveHandler(mapCards[i - 1], findCreateMapPins[i])
+      })
+      let popupClose = mapCards[i - 1].querySelector(".popup__close")
+      popupClose.addEventListener("click", function () {
         PopupInactiveHandler(mapCards[i - 1], findCreateMapPins[i])
-      }
-    })
+      })
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          PopupInactiveHandler(mapCards[i - 1], findCreateMapPins[i])
+        }
+      })
+    }
   }
-}
 
-let k = true
-addressInput.value = getAddressFormAnnoucement()
-function mapActiveHandler() {
+  let k = true
   addressInput.value = getAddressFormAnnoucement()
-  if (k) {
-    map.classList.remove("map--faded")
-    formAnnoucementActiveHandler()
-    formElementActiveHandler(adFormFieldsets)
-    formElementActiveHandler(mapFiltersSelects)
-    formElementActiveHandler(mapFiltersFieldset)
-    createFragment(listObjects)
-    findCreateMapPins = map.querySelectorAll(".map__pin")
-    console.log(findCreateMapPins)
-    createMapPinAnnoucements()
-    let mapCards = map.querySelectorAll(".map__card")
-    mapPinClickHandler(mapCards)
-    k = false
-  }
-}
 
-mapPinMain.addEventListener('mousedown', function (evt){
-  let startCoordinates = {
-    x: evt.clientX,
-    y: evt.clientY
-  }
-  function mouseMoveHandler (moveEvt){
-    let shiftCoordinates = {
-      x: startCoordinates.x - moveEvt.clientX,
-      y: startCoordinates.y - moveEvt.clientY
-    }
-    startCoordinates = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
-    }
-    if (mapPinMain.offsetLeft < 0){
-      mapPinMain.style.left = 1 + "px"
-    } else if (mapPinMain.offsetTop < 90){
-      mapPinMain.style.top = 91 + "px"
-    } else if (mapPinMain.offsetLeft + mainPinWidth > mapPinMain.parentElement.clientWidth){
-      mapPinMain.style.left = (mapPinMain.parentElement.clientWidth - mainPinWidth - 1) + "px"
-    } else if (mapPinMain.offsetTop + mainPinHeight > mapPinMain.parentElement.clientHeight){
-      mapPinMain.style.top = (mapPinMain.parentElement.clientHeight - mainPinHeight - 1) + "px"
-    } else {
-      mapPinMain.style.left = (mapPinMain.offsetLeft - shiftCoordinates.x) + "px"
-      mapPinMain.style.top = (mapPinMain.offsetTop - shiftCoordinates.y) + "px"
+  function mapActiveHandler() {
+    addressInput.value = getAddressFormAnnoucement()
+    if (k) {
+      map.classList.remove("map--faded")
+      formAnnoucementActiveHandler()
+      formElementActiveHandler(adFormFieldsets)
+      formElementActiveHandler(mapFiltersSelects)
+      formElementActiveHandler(mapFiltersFieldset)
+      createFragment(listObjects)
+      findCreateMapPins = map.querySelectorAll(".map__pin")
+      console.log(findCreateMapPins)
+      createMapPinAnnoucements()
+      let mapCards = map.querySelectorAll(".map__card")
+      mapPinClickHandler(mapCards)
+      k = false
     }
   }
-  document.addEventListener('mousemove', mouseMoveHandler)
-  function mouseUpHandler (upEvt) {
-    mapActiveHandler()
-    document.removeEventListener("mousemove", mouseMoveHandler)
-    document.removeEventListener("mouseup", mouseUpHandler)
-  }
-  document.addEventListener("mouseup", mouseUpHandler)
-})
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    let startCoordinates = {
+      x: evt.clientX,
+      y: evt.clientY
+    }
+
+    function mouseMoveHandler(moveEvt) {
+      let shiftCoordinates = {
+        x: startCoordinates.x - moveEvt.clientX,
+        y: startCoordinates.y - moveEvt.clientY
+      }
+      startCoordinates = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      }
+      if (mapPinMain.offsetLeft < 0) {
+        mapPinMain.style.left = 1 + "px"
+      } else if (mapPinMain.offsetTop < 90) {
+        mapPinMain.style.top = 91 + "px"
+      } else if (mapPinMain.offsetLeft + mainPinWidth > mapPinMain.parentElement.clientWidth) {
+        mapPinMain.style.left = (mapPinMain.parentElement.clientWidth - mainPinWidth - 1) + "px"
+      } else if (mapPinMain.offsetTop + mainPinHeight > mapPinMain.parentElement.clientHeight) {
+        mapPinMain.style.top = (mapPinMain.parentElement.clientHeight - mainPinHeight - 1) + "px"
+      } else {
+        mapPinMain.style.left = (mapPinMain.offsetLeft - shiftCoordinates.x) + "px"
+        mapPinMain.style.top = (mapPinMain.offsetTop - shiftCoordinates.y) + "px"
+      }
+    }
+
+    document.addEventListener('mousemove', mouseMoveHandler)
+
+    function mouseUpHandler(upEvt) {
+      mapActiveHandler()
+      document.removeEventListener("mousemove", mouseMoveHandler)
+      document.removeEventListener("mouseup", mouseUpHandler)
+    }
+
+    document.addEventListener("mouseup", mouseUpHandler)
+  })
+})()
+
 
