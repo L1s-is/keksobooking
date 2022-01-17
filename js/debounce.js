d1844d1c1eed71894fb3ec51659534b61e749bd7
Елизавета <86424489+L1s-is@users.noreply.github.com>
2022-01-17 16:22:07 +0500

    исправила ошибку в файле debounce.js

--- js/debounce.js
+++ js/debounce.js
@@ -2,14 +2,13 @@
 
 (function () {
   window.debounce = debounce
-
+  let lastTimeout = null;
   /**
    debounce - Откладывает выполнение функции callback на время interval
    и предотвращает 'дребезг' при повтороном обращении к фукнции callback раньше,
    чем через время interval.
    */
   function debounce(callback, interval) {
-    let lastTimeout;
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
     }
